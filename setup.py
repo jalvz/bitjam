ee2f2c7c8e18cb1193ae79a8e9a285b3ec245710
Juan A <juan@opbeat.com>
2016-06-26 20:50:07 +0200

    setup stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name='bitjam',
+  packages=['bitjam'],
+  version='0.1',
+  description='A simple traffic congestion monitor',
+  author='Juan Alvarez',
+  author_email='ihavetheblues@gmail.com',
+  url='git@github.com:jalvz/bitjam.git',
+  keywords=['back-pressure', 'rate-limiting'],
+  classifiers=[],
+)
